5e36f6d59d2c4cddaa98d7ba51f09230005d3eaa
MacBook Pro Magda <magda@ssolutions.com.pl>
2018-01-21 09:26:06 +0100

    add User

---
+++ src/main/java/pl/sda/ebook/domain/User.java
@@ -0,0 +1,12 @@
+package pl.sda.ebook.domain;
+
+public class User {
+
+    private String login;
+    private String psw;
+
+    public User(String login, String psw) {
+        this.login = login;
+        this.psw = psw;
+    }
+}
